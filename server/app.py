084d8f5a9224d8309458cbb6e78d2f9ffff5a895
annemusau <moigeanne646@gmail.com>
2024-10-10 23:17:19 +0300

    commiting all

--- server/app.py
+++ server/app.py
@@ -17,11 +17,33 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+   def get(self):
+        plants = Plant.query.all()
+        plants_data = [plant.to_dict() for plant in plants]
+        return make_response(jsonify(plants_data), 200)
+   def post(self):
+        data = request.get_json()
+
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price']
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+
+        return make_response(jsonify(new_plant.to_dict()), 201)
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, plant_id):
+        plant = Plant.query.get(plant_id)
+        if plant:
+            return make_response(jsonify(plant.to_dict()), 200)
+        return make_response(jsonify({'error': 'Plant not found'}), 404)
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:plant_id>')
+  
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
